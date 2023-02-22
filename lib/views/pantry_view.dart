fd21b78b0d25f1d147ff02c516b754dcc81ebb05
Farina MAhboob <W0766260@myscc.ca>
2023-02-22 00:51:18 -0500

    Created main view of pantry screen

--- lib/views/pantry_view.dart
+++ lib/views/pantry_view.dart
@@ -14,63 +14,108 @@ class PantryView extends StatelessWidget {
             centerTitle: true,
             backgroundColor: const Color(0xff459657),
             title: const Text('Pocket Kitchen'),
+            actions: [
+              IconButton(
+            onPressed: () {
 
+            },
+            icon: const Icon(Icons.exit_to_app_sharp),
+            tooltip: 'Sign Out',
+          ),
+    ]
         ),
-        body: Container(
+        body: Column(
+          children: <Widget> [
+           TextField(
+             onChanged: (value){
 
+             },
+             controller: TextEditingController(),
+             decoration: const InputDecoration(
+                 hintText: "Search",
+                 prefixIcon: Icon(Icons.search, color: Colors.black38,),
+                 enabledBorder: OutlineInputBorder(
+                   borderSide: BorderSide(
+                     width: 2.0,
+                     color: Colors.grey,
+                   ),
+                 ),
+
+               focusedBorder: OutlineInputBorder(
+                 borderSide: BorderSide(width: 2.0, color: Colors.black38,)
+               )
+
+             ),
+           ),
+            const Text("Available", style: TextStyle(fontSize: 15, color: Color(0xff459657)),textAlign: TextAlign.start,),
+            Expanded(child: ListView.builder(itemBuilder: (context, index){
+              return const Card(
+
+              );
+            }
+            )
+            ),
+            const Text("Unavailable"),
+            Expanded(child: ListView.builder(itemBuilder: (context, index){
+              return const Card(
+
+              );
+            }
+            )
+            )
+
+          ],
         ),
-        drawer: Container(
-          child: Drawer(
-              elevation: 1.5,
-              child: Column(children: <Widget>[
-                DrawerHeader(
-                    decoration: const BoxDecoration(
-                      color: Colors.transparent,
-                    ), child: Image.asset("lib/assets/logo.png")),
-                Expanded(
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      children: <Widget>[
-                        ListTile(
-                          title: const Text('Create Pantry', style: drawerStyle,),
-                          leading: const Icon(Icons.add_circle_outline, color: drawerIcon,),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          title: const Text('Join Pantry', style: drawerStyle,),
-                          leading: const Icon(Icons.exit_to_app, color: drawerIcon,),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                            title: const Text('Delete Pantry', style: drawerStyle,),
-                            leading: const Icon(Icons.delete_forever_rounded, color: drawerIcon,),
-                            onTap: () {}
-                        ),
-                        ListTile(
-                            title: const Text('Leave Pantry', style: drawerStyle,),
-                            leading: const Icon(Icons.arrow_back, color: drawerIcon,),
-                            onTap: () {}
-                        ),
-                        ListTile(
-                            title: const Text('Switch Pantry', style: drawerStyle,),
-                            leading: const Icon(Icons.swap_calls, color: drawerIcon,),
-                            onTap: () {}
-                        )
-                      ],
-                    )),
-               const Divider(),
-                Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 100,
-                    child: const ListTile(
-                        title: Text("pantryName!", style: drawerStyle,),
-                        leading: Icon(Icons.desktop_windows_sharp, color: drawerIcon,),
-                    )
+        drawer: Drawer(
+            elevation: 1.5,
+            child: Column(children: <Widget>[
+              DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ), child: Image.asset("lib/assets/logo.png")),
+              Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      ListTile(
+                        title: const Text('Create Pantry', style: drawerStyle,),
+                        leading: const Icon(Icons.add_circle_outline, color: drawerIcon,),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Join Pantry', style: drawerStyle,),
+                        leading: const Icon(Icons.exit_to_app, color: drawerIcon,),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                          title: const Text('Delete Pantry', style: drawerStyle,),
+                          leading: const Icon(Icons.delete_forever_rounded, color: drawerIcon,),
+                          onTap: () {}
+                      ),
+                      ListTile(
+                          title: const Text('Leave Pantry', style: drawerStyle,),
+                          leading: const Icon(Icons.arrow_back, color: drawerIcon,),
+                          onTap: () {}
+                      ),
+                      ListTile(
+                          title: const Text('Switch Pantry', style: drawerStyle,),
+                          leading: const Icon(Icons.swap_calls, color: drawerIcon,),
+                          onTap: () {}
+                      )
+                    ],
+                  )),
+             const Divider(),
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 100,
+                  child: const ListTile(
+                      title: Text("pantryName!", style: drawerStyle,),
+                      leading: Icon(Icons.desktop_windows_sharp, color: drawerIcon,),
+                  )
 
-                ),
+              ),
 
-              ])),
-        ),
+            ])),
 
     );
   }
