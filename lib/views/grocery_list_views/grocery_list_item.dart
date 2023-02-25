906581d9405a44b0b909730ad78541e538e2a8e6
Robbie Brush <W0741993@myscc.ca>
2023-02-24 22:39:07 -0500

    Added expansion functionality to grocery list items, created expanded UI

--- lib/views/grocery_list_views/grocery_list_item.dart
+++ lib/views/grocery_list_views/grocery_list_item.dart
@@ -44,23 +44,104 @@ class GroceryListItemState extends State<GroceryListItem> {
                           color: Color(0xff7B7777),
                         ),
                         duration: const Duration(milliseconds: 100),
-                        height: isExpanded ? 175 : 30,
+                        height: isExpanded ? 175 : 40,
                         child:
                             Padding(
-                              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
                               child:
                                   Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: const [
-                                      Text(
-                                          "Carrots",
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                              fontSize: 20,
+                                    children: [
+                                      Visibility(
+                                        visible: isExpanded,
+                                        child: Padding(
+                                          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+                                          child: Container(
+                                            height: 159,
+                                            width: 159,
+                                            decoration: const BoxDecoration(
+                                              borderRadius: BorderRadius.all(Radius.circular(5)),
                                               color: Colors.white,
-                                              fontWeight: FontWeight.w500
-                                          )
-                                      )
+                                            ),
+                                            child: Image.network("https://www.pngmart.com/files/5/Ketchup-PNG-HD.png"),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 4,
+                                        child:
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              constraints: BoxConstraints(
+                                                maxWidth: MediaQuery.of(context).size.width - 32.0,
+                                              ),
+                                              child: Text(
+                                                "Heinz Tomato Ketchup, 750mL/25oz., Bottle, {Imported From Canada",
+                                                textAlign: TextAlign.left,
+                                                style: const TextStyle(
+                                                  fontSize: 20,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500,
+                                                ),
+                                                maxLines: isExpanded ? 4 : 1,
+                                                overflow: isExpanded ? TextOverflow.ellipsis: TextOverflow.fade,
+                                              ),
+                                            ),
+                                            Visibility(
+                                              visible: isExpanded,
+                                              child: const Spacer(),
+                                            ),
+                                            Visibility(
+                                              visible: isExpanded,
+                                              child: const Padding(
+                                                padding: EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 0.0),
+                                                child: Text(
+                                                  "Vegetable, organic, classic, yummy",
+                                                  textAlign: TextAlign.left,
+                                                  style: TextStyle(
+                                                      fontSize: 18,
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight.w500
+                                                  ),
+                                                  maxLines: 2,
+                                                  overflow: TextOverflow.ellipsis,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Expanded(
+                                          flex: isExpanded ? 1 : 0,
+                                          child: Column(
+                                            children: [
+                                              Visibility(
+                                                visible: isExpanded,
+                                                child: const Spacer(),
+                                              ),
+                                              Visibility(
+                                                visible: isExpanded,
+                                                child: Container(
+                                                  height: 40,
+                                                  width: 40,
+                                                  decoration: const BoxDecoration(
+                                                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                                                    color: Colors.white,
+                                                  ),
+                                                  child: IconButton(
+                                                    icon: const Icon(Icons.check),
+                                                    color: const Color(0xff459657),
+                                                    tooltip: 'Manually restock this item',
+                                                    onPressed: () {
+                                                      //manual restock logic
+                                                    },
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                      ),
                                     ],
                                   ),
                             ),
