eeda9729fdfb0a1b51c5a420c2369b4b2b44aae6
Farina MAhboob <W0766260@myscc.ca>
2023-02-27 20:18:11 -0500

    Added percent left, Text field and apply button in Pantry Item

--- lib/views/pantry_list_views/pantry_list_item.dart
+++ lib/views/pantry_list_views/pantry_list_item.dart
@@ -32,10 +32,10 @@ class PantryListItemState extends State<PantryListItem> {
                 AnimatedContainer(
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(5)),
-                    color: Color(0xff7B7777),
+                    color: Color(0xff459657),
                   ),
                   duration: const Duration(milliseconds: 100),
-                  height: isExpanded ? 175 : 40,
+                  height: isExpanded ? 250 : 40,
                   child:
                   Padding(
                     padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
@@ -61,7 +61,8 @@ class PantryListItemState extends State<PantryListItem> {
                           flex: 4,
                           child:
                           Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
                               Container(
                                 constraints: BoxConstraints(
@@ -79,10 +80,9 @@ class PantryListItemState extends State<PantryListItem> {
                                   overflow: isExpanded ? TextOverflow.ellipsis: TextOverflow.fade,
                                 ),
                               ),
-                              Visibility(
-                                visible: isExpanded,
-                                child: const Spacer(),
-                              ),
+
+
+
                               Visibility(
                                 visible: isExpanded,
                                 child: const Padding(
@@ -100,9 +100,66 @@ class PantryListItemState extends State<PantryListItem> {
                                   ),
                                 ),
                               ),
+                              Visibility(
+                                visible: isExpanded,
+                                child:  const Padding(
+                                  padding: EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 0.0),
+                                  child: Text(
+                                    "Percentage Left",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500
+                                    ),
+                                  ),
+                                ),
+
+                              ),
+                              Visibility(
+                                visible: isExpanded,
+                                child:   Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                Container(
+                                  height: 30,
+                                  width: 50,
+
+                                  child: TextField(
+                                    enabled: true,
+                                    onChanged: (String input) {
+                                    },
+                                    keyboardType: TextInputType.number,
+                                    decoration: const InputDecoration(
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border: OutlineInputBorder(
+                                        borderSide: BorderSide.none
+                                      )
+                                    ),
+                                  ),
+                                ),
+
+                    TextButton(
+                                      onPressed: () {},
+                                      child: Container(
+                                        color: Colors.white,
+                                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                                        child: const Text(
+                                          'Apply',
+                                          style: TextStyle(color: Color(0xff459657), fontSize: 13.0),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                              ),
+
                             ],
                           ),
                         ),
+
                       ],
                     ),
                   ),
