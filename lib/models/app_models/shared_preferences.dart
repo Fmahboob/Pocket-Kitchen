0ef8793273e3e06ec511ecbb62b9bb7c083957e1
Robbie Brush <W0741993@myscc.ca>
2023-03-15 17:59:04 -0400

    Added getters and setters for currentPantry and all pantries in shared prefs model

--- lib/models/app_models/shared_preferences.dart
+++ lib/models/app_models/shared_preferences.dart
@@ -9,12 +9,33 @@ class SharedPrefs {
     }
   }
 
+  //userId getter
   String get userId => _sharedPrefs!.getString("userId") ?? "";
-
+  //userId setter
   set userId(String value) {
     _sharedPrefs!.setString("userId", value);
   }
 
+  //currentPantry getter
+  String get currentPantry => _sharedPrefs!.getStringList("pantries")?[0] ?? "";
+  //currentPantry setter
+  updateCurrentPantry(String value) {
+    _sharedPrefs!.getStringList("pantries")?.insert(0, value);
+  }
+
+  //all pantries getter
+  List<String> get pantries => _sharedPrefs!.getStringList("pantries") ?? [];
+  //new pantry setter
+  addNewPantry(String value) {
+    String pantry1 = _sharedPrefs!.getStringList("pantries")?[0] ?? "";
+    String pantry2 = _sharedPrefs!.getStringList("pantries")?[1] ?? "";
+
+    _sharedPrefs!.setString("pantries"[0], value);
+    _sharedPrefs!.setString("pantries"[1], pantry1);
+    _sharedPrefs!.setString("pantries"[2], pantry2);
+  }
+
+  //getter for if user is signed in
   bool get signedIn {
     if (userId == "") {
       return false;
@@ -24,4 +45,5 @@ class SharedPrefs {
   }
 }
 
+//singleton
 final sharedPrefs = SharedPrefs();
