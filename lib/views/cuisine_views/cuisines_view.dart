4a0bdc850378add362137a6d05ddd29560385368
Farina MAhboob <W0766260@myscc.ca>
2023-03-24 08:17:32 -0400

    Created Ui for Cuisines static List Screen

--- lib/views/cuisine_views/cuisines_view.dart
+++ lib/views/cuisine_views/cuisines_view.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
 
-class CuisinesView extends StatelessWidget {
-  const CuisinesView({Key? key}) : super (key: key);
 
+class CuisinesView extends StatefulWidget {
+  const CuisinesView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => CuisinesViewState();
+}
+
+class CuisinesViewState extends State<CuisinesView> {
+  String pantryName = "Robbie's Home";
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: const Color(0xff459657),
-            title: const Text('Recipes'),
-        ),
-        body: Container(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xff459657),
+        title: Text(pantryName),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+            child: Text(
+                "Cuisines",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Color(0xff459657),
+                    fontWeight: FontWeight.w600
+                )
+            ),
+          ),
 
-        )
+          Expanded(
+
+            child: ListView.builder(
+
+              itemBuilder: (context, index) {
+                return Container();
+
+              },
+
+            ),
+          ),
+
+
+        ],
+      ),
     );
   }
 }
+
+
+
+
+
