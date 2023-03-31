7180251a1a58134e72c28a1ab87b7af4a31152c2
Farina MAhboob <W0766260@myscc.ca>
2023-03-31 16:54:49 -0400

    Created Recipe class to show detailed recipe

---
+++ lib/views/cuisine_views/recipe_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class RecipeView extends StatefulWidget {
+  const RecipeView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => RecipeViewState();
+
+}
+
+class RecipeViewState extends State<RecipeView>{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
