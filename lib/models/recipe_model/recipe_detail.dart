e2bb41bd3327a1286d3fd77c50de9ed5c8e03c73
Farina MAhboob <W0766260@myscc.ca>
2023-03-31 17:35:21 -0400

    Created Recipe Detail model for detailed recipes

---
+++ lib/models/recipe_model/recipe_detail.dart
@@ -0,0 +1,12 @@
+class RecipeDetail {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String cookTime;
+  final int ingredientUnit;
+  final String ingredientName;
+  final String instructions;
+
+  RecipeDetail(this.id, this.title, this.imageUrl, this.cookTime, this.ingredientUnit, this.ingredientName, this.instructions);
+
+}
