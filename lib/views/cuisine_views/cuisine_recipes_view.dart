fe916185db98337e6034275d9bd7a4733d58de86
Farina MAhboob <W0766260@myscc.ca>
2023-03-31 01:20:58 -0400

    Added colors and box ecoration

--- lib/views/cuisine_views/cuisine_recipes_view.dart
+++ lib/views/cuisine_views/cuisine_recipes_view.dart
@@ -59,17 +59,33 @@ class CuisinesRecipesViewState extends State<CuisinesRecipesView>{
                   itemCount: recipes.length,
                   itemBuilder: (context, index) {
                     final RecipeList recipe = recipes[index];
-                    return Card(
-                      child: Column(
+                    return  Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
 
 
-                          Text(recipe.title),
-                          Image.network(recipe.imageUrl),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
+                              color: Color(0xff459657),
+
+                            ), 
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 4.0, 4.0),
+                              child: Text(recipe.title, style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500
+                              ),),
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(8.0, 4.0, 4.0, 4.0),
+                              child: Image.network(recipe.imageUrl)),
                         ],
-                      ),
-                    );
+                      );
+
                   },
                 ),
               ),
