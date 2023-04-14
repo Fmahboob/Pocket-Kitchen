d7216bce467893caa43773cc12a419e09e696f61
Robbie Brush <W0741993@myscc.ca>
2023-04-13 20:13:24 -0400

    Colored and customized recipe ingredients based on availability in users pantry

--- lib/views/cuisine_views/recipe_view.dart
+++ lib/views/cuisine_views/recipe_view.dart
@@ -1,8 +1,10 @@
 
 import 'package:flutter/material.dart';
-import 'package:pocket_kitchen/models/recipe_model/recipe_detail.dart';
 
 import '../../models/app_models/shared_preferences.dart';
+import '../../models/data_models/food.dart';
+import '../../models/data_models/pantry_food.dart';
+import '../../models/recipe_model/recipe_detail.dart';
 
 class RecipeView extends StatefulWidget {
   final RecipeDetail recipeDetail;
@@ -14,9 +16,117 @@ class RecipeView extends StatefulWidget {
 }
 
 class RecipeViewState extends State<RecipeView>{
-  static const textStyle = TextStyle(fontSize: 20, color: Color(0xff7B7777));
-  static const headingStyle = TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.w500
+  static const textStyle = TextStyle(fontSize: 20, color: Color(0xff7B7777), fontWeight: FontWeight.w500);
+  static const headingStyle = TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500
   );
+
+  String availabilityStr = "";
+  late Color ingredientColor;
+  String availabilityChar = "";
+
+  int ingredientInPantry(ExtendedIngredients ingredient) {
+    for (PantryFood pantryFood in sharedPrefs.pantryFoodList) {
+      for (Food food in sharedPrefs.foodList) {
+        if (pantryFood.foodId == food.id) {
+          if (food.name!.toLowerCase().contains(ingredient.name.toLowerCase())) {
+            if (food.ownUnit == "0") {
+              double weight = double.parse(food.weight!);
+              double percent = double.parse(pantryFood.amount!);
+              double currAmount = weight * percent;
+
+              return convertUnitsFromKg(currAmount, ingredient.unit, ingredient.amount);
+            } else {
+              return convertUnitsFromCups(double.parse(pantryFood.amount!), ingredient.unit, ingredient.amount);
+            }
+          }
+        }
+      }
+    }
+    return 1;
+  }
+
+  int convertUnitsFromKg(double currAmount, String unit, double recipeAmount) {
+    unit = unit.toLowerCase();
+    if (unit == "oz" || unit == "ounces" || unit == "ounce") {
+      double amountInOz = currAmount * 35.274;
+      if (amountInOz >= recipeAmount) {
+        return 0;
+      }  else {
+        return 1;
+      }
+    } else if (unit == "tsp" || unit == "teaspoon" || unit == "teaspoons" || unit == "tea spoon" || unit == "tea spoons") {
+      double amountInTsp = currAmount * 204.082;
+      if (amountInTsp >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "tbsp" || unit == "tablespoon" || unit == "tablespoons" || unit == "table spoon" || unit == "table spoons") {
+      double amountInTbsp = currAmount * 67.568;
+      if (amountInTbsp >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "cup" || unit == "cups" || unit == "serving" || unit == "servings" || unit == "can" || unit == "cans" || unit.contains("handful")) {
+      double amountInCups = currAmount * 6;
+      if (amountInCups >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "lbs." || unit == "lbs" || unit == "lb" || unit == "lb." || unit == "pounds" || unit == "pound") {
+      double amountInLbs = currAmount * 2.205;
+      if (amountInLbs >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else {
+      return 2;
+    }
+  }
+
+  int convertUnitsFromCups(double currAmount, String unit, double recipeAmount) {
+    unit = unit.toLowerCase();
+    if (unit == "oz" || unit == "ounces" || unit == "ounce") {
+      double amountInOz = currAmount * 8;
+      if (amountInOz >= recipeAmount) {
+        return 0;
+      }  else {
+        return 1;
+      }
+    } else if (unit == "tsp" || unit == "teaspoon" || unit == "teaspoons" || unit == "tea spoon" || unit == "tea spoons") {
+      double amountInTsp = currAmount * 48;
+      if (amountInTsp >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "tbsp" || unit == "tablespoon" || unit == "tablespoons" || unit == "table spoon" || unit == "table spoons") {
+      double amountInTbsp = currAmount * 16;
+      if (amountInTbsp >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "cup" || unit == "cups" || unit == "serving" || unit == "servings" || unit == "can" || unit == "cans" || unit.contains("handful")) {
+      if (currAmount >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (unit == "lbs." || unit == "lbs" || unit == "lb" || unit == "lb." || unit == "pounds" || unit == "pound") {
+      double amountInLbs = currAmount / 2;
+      if (amountInLbs >= recipeAmount) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else {
+      return 2;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,13 +165,13 @@ class RecipeViewState extends State<RecipeView>{
               children:[
                 const Padding(
                   padding: EdgeInsets.fromLTRB(8.0, 4.0, 4.0, 4.0),
-                  child: Text("Cook Time",
+                  child: Text("Cook Time:",
                     style: textStyle,),
                 ),
 
               Padding(
                 padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
-                child: Text("${recipeDetail.cookTime}min",
+                child: Text("${recipeDetail.cookTime} mins.",
                   style: textStyle,),
               ),
               ],
@@ -84,7 +194,7 @@ class RecipeViewState extends State<RecipeView>{
                     Spacer(),
                     Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text("Available",
+                      child: Text("Availability",
                         style: headingStyle,),
                     ),
 
@@ -100,23 +210,49 @@ class RecipeViewState extends State<RecipeView>{
                     itemCount: recipeDetail.extendedIngredients.length,
                     itemBuilder: (context, index) {
                       final ingredient = recipeDetail.extendedIngredients[index];
+                      if (ingredientInPantry(ingredient) == 0) {
+                        availabilityStr = "Available";
+                        availabilityChar = "\u2713";
+                        ingredientColor = const Color(0xff459657);
+                      } else if (ingredientInPantry(ingredient) == 1) {
+                        availabilityStr = "Unavailable";
+                        availabilityChar = "X";
+                        ingredientColor = const Color(0xff9E4848);
+                      } else if (ingredientInPantry(ingredient) == 2) {
+                        availabilityStr = "Unknown Unit";
+                        availabilityChar = "?";
+                        ingredientColor = const Color(0xfff5d16e);
+                      }
                       return Row(
                         children: [
                           Padding(
-                            padding: EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
-                            child: Text("${ingredient.name}  ${ingredient.amount}  ${ingredient.unit}"),
+                            padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
+                            child: Text(
+                                "${ingredient.name}  ${ingredient.amount}  ${ingredient.unit}",
+                                style: TextStyle(
+                                  color: ingredientColor
+                                ),
+                            ),
 
                           ),
                           Padding(
-                            padding: EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
-                            child: Text("X",
-                              style: textStyle,),
+                            padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
+                            child: Text(availabilityChar,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: ingredientColor,
+                                  fontWeight: FontWeight.w500)
+                            ),
                           ),
-                          Spacer(),
+                          const Spacer(),
                           Padding(
-                            padding: EdgeInsets.fromLTRB(4.0, 4.0, 8.0, 4.0),
-                            child: Text("Available",
-                              style: textStyle,),
+                            padding: const EdgeInsets.fromLTRB(4.0, 4.0, 8.0, 4.0),
+                            child: Text(availabilityStr,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: ingredientColor,
+                                  fontWeight: FontWeight.w500
+                              )),
                           ),
 
                         ],
